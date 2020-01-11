16fdef8c95198c55b80604b3da96b6133450ef3d
XikkeN <e.humphreys@tricentis.com>
2020-01-11 18:47:07 -0500

    completed the rewrite and added color evaluator (not working) and the save/data retrieval working.

--- script.js
+++ script.js
@@ -1,76 +1,61 @@
-var time = moment().format("HH");
-// console.log(time)
-
-var workingHours = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ];
-
-for (var i = 0; i < workingHours.length; i++) {
-    var rowDiv = document.createElement("div");
-    document.getElementsByClassName("container")[0].appendChild(rowDiv);
-    rowDiv.setAttribute("class", "row");
-    var colDiv = document.createElement("div");
-    document.getElementsByClassName("row")[i].appendChild(colDiv);
-    colDiv.setAttribute("class", "col-lg-12 textInput");
-    var inputGroupDiv = document.createElement("div");
-    document.getElementsByClassName("col-lg-12")[i].appendChild(inputGroupDiv);
-    inputGroupDiv.setAttribute("class", "input-group");
-    var inputGroupPrependDiv = document.createElement("div");
-    document.getElementsByClassName("input-group")[i].appendChild(inputGroupPrependDiv);
-    inputGroupPrependDiv.setAttribute("class", "input-group-prepend");
-    var inputGroupTextSpan = document.createElement("span");
-    document.getElementsByClassName("input-group-prepend")[i].appendChild(inputGroupTextSpan);
-    inputGroupTextSpan.setAttribute("class", "input-group-text hour");
-    inputGroupTextSpan.innerText = workingHours[i]
-    var formControlTextArea = document.createElement("textarea");
-    document.getElementsByClassName("input-group")[i].appendChild(formControlTextArea);
-    formControlTextArea.setAttribute("class", "form-control");
-    var inputGroupAppendDiv = document.createElement("div");
-    document.getElementsByClassName("input-group")[i].appendChild(inputGroupAppendDiv);
-    inputGroupAppendDiv.setAttribute("class", "input-group-append button-addon4");
-    var saveButton = document.createElement("button");
-    document.getElementsByClassName("input-group-append")[i].appendChild(saveButton);
-    saveButton.setAttribute("class", "btn btn-success saveBtn");
-    var saveIcon = document.createElement("i");
-    document.getElementsByClassName("saveBtn")[i].appendChild(saveIcon);
-    saveIcon.setAttribute("class", "far fa-save");
-
-
-    var timeParsed = parseInt(time)
-    // console.log(timeParsed)
-    // console.log(workingHours[i])
-
-    if (workingHours[i] < timeParsed) {
-    document.getElementsByClassName("form-control")[i].setAttribute("class", "form-control past");
+function retrieveNote(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
     }
-    if (workingHours[i] === timeParsed) {
-    document.getElementsByClassName("form-control")[i].setAttribute("class", "form-control present");
-    }
-    if (workingHours[i] > timeParsed) {
-    document.getElementsByClassName("form-control")[i].setAttribute("class", "form-control future");
-    }
+}
 
-    saveButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        var note = document.getElementsByClassName("form-control")[i].value;
-        console.log(note)
-        localStorage.setItem("note", note);
-        renderSavedNotes();
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    for (let i = 9; i < 18; i++) {
+        let rowDiv = $(`<div id='${i}' data-time=${i} class="row">`);
+        let colDiv = $(`<div class="col-lg-12 textInput">`);
+        let inputGroupDiv = $(`<div class="input-group">`);
+        let inputGroupPrependDiv = $(`<div class="input-group-prepend">`);
+        let inputGroupTextSpan = $(`<span class="input-group-text hour">`);
+        let formControlTextArea = $(`<textarea id='text${i}' class="form-control past">`);
+        let inputGroupAppendDiv = $(`<div id="button-addon4" class="input-group-append">`);
+        let saveButton = $(`<button id=${i} class="btn btn-success saveBtn">`);
+        let saveIcon = $(`<i div id='${i}' class="far fa-save">`);
+        let hourIndex = formatAMPM(i);
+        let time = parseInt((moment().format("HH")));
+        let hour = i;
+
+        $(".container").append(rowDiv);
+        rowDiv.append(colDiv);
+        colDiv.append(inputGroupDiv);
+        inputGroupDiv.append(inputGroupPrependDiv);
+        inputGroupPrependDiv.append(inputGroupTextSpan);
+        inputGroupDiv.append(formControlTextArea);
+        inputGroupDiv.append(inputGroupAppendDiv);
+        inputGroupAppendDiv.append(saveButton);
+        saveButton.append(saveIcon);
+        rowDiv.append(colDiv);
+        inputGroupTextSpan.text(hourIndex);
+        retrieveNote(i);
+
+        function formatAMPM(hourIndex) {
+            var ampm = hourIndex >= 12 ? 'pm' : 'am';
+            hourIndex = hourIndex % 12;
+            hourIndex = hourIndex ? hourIndex : 12;
+            return hourIndex + ampm;
+        }
+
+        console.log(hour, time);
+
+        function updateColors() {
+            if (hour === time) {
+                $(`#text${i}`).attr("class", "form-control present");
+            } else if (hour < time) {
+                $(`#text${i}`).attr("class", "form-control future");
+            }
+        }
+        updateColors();
+    }
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function() {
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings('.form-control').val();
+        localStorage.setItem(eventId, eventText);
     });
-};
-
-// function renderSavedNotes {
-//     var savedNote = localStorage.getItem("note");
-//     formControlTextArea.value = savedNote;
-//  };
-
-
-var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-document.getElementById("currentDay").innerText = currentTime;
-
-
-
-
-
-
-
-
-
+});
